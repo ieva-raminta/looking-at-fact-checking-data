8db9fd00c00a2e25f33e43dddf673045641147dc
ieva-raminta <ieva.staliunaite@gmail.com>
2023-01-04 13:53:36 +0000

    parse chaosnli and evaluate with bart

--- chaosNLI/evaluate_bart_mnli_on_chaosnli_parsed.py
+++ chaosNLI/evaluate_bart_mnli_on_chaosnli_parsed.py
@@ -45,8 +45,10 @@ for item in parsed_chaosnli_items:
 
     for result in results:
 
-        highest_confidence = 0
+        highest_confidence = {} 
+        highest_confidence["score"] = {}
         edited_item = None
+        highest_confidence["edited_item"] = {}
 
         premise = result["example"]["premise"]
         hypothesis = result["example"]["hypothesis"]
@@ -58,11 +60,18 @@ for item in parsed_chaosnli_items:
         neutral_percentage = 0
         if "c" in result["label_counter"]: 
             contradiction_percentage = result["label_counter"]["c"]
+            highest_confidence["score"]["c"] = 0
         if "e" in result["label_counter"]: 
             entailment_percentage = result["label_counter"]["e"]
+            highest_confidence["score"]["e"] = 0
         if "n" in result["label_counter"]:
             neutral_counter = result["label_counter"]["n"]
+            highest_confidence["score"]["n"] = 0
     
+        majority_label_confidence = result["label_counter"][majority_label]
+        highest_confidence["score"][majority_label] = 1
+        highest_confidence["edited_item"][majority_label] = (premise, hypothesis, majority_label_confidence, "", "original")
+
         if "subtrees_from_premise" in result.keys(): 
             subtrees_from_premise = result["subtrees_from_premise"]
             subtrees_from_hypothesis = result["subtrees_from_hypothesis"]
@@ -77,14 +86,28 @@ for item in parsed_chaosnli_items:
                 confidence = logits.max().item()
                 predicted_class_id = logits.argmax().item()
                 predicted_label = mnli_labels_to_nli[predicted_class_id]
-                if confidence > highest_confidence and predicted_label != majority_label: 
-                    edit = premise[subtrees_from_premise[cropped_id][0]:] + premise[:subtrees_from_premise[cropped_id][1]]
-                    edited_item = (cropped_premise, hypothesis, predicted_label, edit)
-                    pdb.set_trace()
+                if predicted_label in highest_confidence["score"] and confidence > highest_confidence["score"][predicted_label]: 
+                    edit = premise[subtrees_from_premise[cropped_id][0]:subtrees_from_premise[cropped_id][1]]
+                    edited_item = (cropped_premise, hypothesis, predicted_label, edit, "cropped premise")
+                    highest_confidence["score"][predicted_label] = confidence
+                    highest_confidence["edited_item"][predicted_label] = edited_item
 
-            parsed_dataset.append(
-                    {"premise": cropped_premise, "hypothesis": cropped_hypotheses[cropped_id], "label": majority_label}
-            )
+
+            for cropped_id, cropped_hypothesis in enumerate(cropped_hypotheses):
+                tokenized_premhyp = tokenizer(premise, cropped_hypothesis, return_tensors="pt")
+                with torch.no_grad():
+                    logits = model(**tokenized_premhyp).logits
+                confidence = logits.max().item()
+                predicted_class_id = logits.argmax().item()
+                predicted_label = mnli_labels_to_nli[predicted_class_id]
+                if predicted_label in highest_confidence["score"] and confidence > highest_confidence["score"][predicted_label]:
+                    edit = hypothesis[subtrees_from_premise[cropped_id][0]:subtrees_from_hypothesis[cropped_id][1]]
+                    edited_item = (premise, cropped_hypothesis, predicted_label, edit, "cropped_hypothesis")
+                    highest_confidence["score"][predicted_label] = confidence
+                    highest_confidence["edited_item"][predicted_label] = edited_item
+
+        pdb.set_trace()
+        parsed_dataset.append(edited_item)
 
 
 
